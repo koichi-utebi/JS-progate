8374e6bd0642df75c6393f5ae6ce6df817d8b46a
koichi-utebi <k.utebi.1225@gmail.com>
2022-07-06 12:00:39 +0900

    finish 5

--- Desktop/JS/script.js
+++ Desktop/JS/script.js
@@ -8,3 +8,9 @@ console.log(3 - 2);
 console.log(3 * 7);
 console.log(6 / 3);
 console.log(9 % 2);
+
+console.log("にんじゃ" + "わんこ");
+console.log("A" + "B" + "C");
+
+console.log("3" + "5");
+console.log(3 + 5);
